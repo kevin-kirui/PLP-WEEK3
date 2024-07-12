2f34e7ba8aa109ec4734d79653a132e638825529
kevin-kirui <kipkoechkevin42@gmail.com>
2024-07-12 17:20:09 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+// Declare variables of different data types
+let greeting = "Hello,";
+let name = "World";
+let age = 25;
+let isStudent = true;
+
+// Define functions for basic operations
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+// Testing functions and logging output
+console.log(add(5, 3));       // Output: 8
+console.log(subtract(10, 4)); // Output: 6
+console.log(divide(20, 5));   // Output: 4
+console.log(multiply(7, 2));  // Output: 14
